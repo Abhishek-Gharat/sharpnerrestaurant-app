9d9e6390c610322fa9f59840ca40cd8f48d3cbce
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2026-05-01 23:59:34 +0530

    meal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <MealsSummary />
+      </main>
+    </>
+  );
 }
 
 export default App;
