6495af970107286f06218d90af766c0dc29a703c
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2026-05-02 00:51:12 +0530

    Refactor Cart component; add CartItem component for individual cart items and update styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,35 @@
 import { useContext } from 'react';
 
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   return (
     <Modal onClose={props.onCloseCart}>
       <ul className={classes['cart-items']}>
         {cartCtx.items.map((item) => (
-          <li className={classes['cart-item']} key={item.id}>
-            <span className={classes.name}>{item.name}</span>
-            <span className={classes.meta}>
-              {item.amount} x ${item.price.toFixed(2)}
-            </span>
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
         ))}
       </ul>
       <div className={classes.total}>
@@ -28,7 +40,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']} onClick={props.onCloseCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
